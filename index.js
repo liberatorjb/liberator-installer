b9a8c7f67dc410f8998218e81d5bd2fd71dfd2b3
Evan <41494790+evanlol@users.noreply.github.com>
2019-08-20 14:54:19 -0400

    Added a non-functional UI

---
+++ index.js
@@ -0,0 +1,20 @@
+const { app, BrowserWindow } = require('electron')
+
+// Define the electron app
+function createWindow () {
+  let win = new BrowserWindow({
+    width: 400,
+    height: 200,
+    webPreferences: {
+      nodeIntegration: true
+    },
+    resizable: false,
+    minimizable: false,
+    setBackgroundColor: '#323234'
+  })
+  win.removeMenu(); // Removes the menu bar from the application because it's not needed (for now)
+  // The html file will be index.html
+  win.loadFile('index.html')
+}
+
+app.on('ready', createWindow)
